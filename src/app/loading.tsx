386b89ec8e14878041f8c6f6fd73576a27baad74
eung-eung <boyvip5231@gmail.com>
2024-01-21 17:38:20 +0700

    change loading for test

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -5,7 +5,8 @@ import LoadingTheme from './components/loadingTheme/loadingTheme'
 export default function Loading() {
     return (
         <div>
-            <LoadingTheme />
+            {/* <LoadingTheme /> */}
+            <img src='https://file1.dangcongsan.vn/DATA/0/2018/10/68___gi%E1%BA%BFng_l%C3%A0ng_qu%E1%BA%A3ng_ph%C3%BA_c%E1%BA%A7u__%E1%BB%A9ng_h%C3%B2a___%E1%BA%A3nh_vi%E1%BA%BFt_m%E1%BA%A1nh-16_51_07_908.jpg' />
         </div>
     )
 }
