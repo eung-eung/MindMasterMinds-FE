e0c014c941374c974a301e9870e02523affff1b3
eung-eung <boyvip5231@gmail.com>
2024-01-27 01:50:32 +0700

    add image picker component

--- src/app/components/explorePage/form-modal.tsx
+++ src/app/components/explorePage/form-modal.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Modal from '@mui/material/Modal';
 import classes from './form-modal.module.css'
 import CancelIcon from '@mui/icons-material/Cancel';
+import FormModalImagePicker from './form-modal-image-picker';
 
 export default function FormModal({ open, handleClose }:
     { open: boolean, handleClose: any }) {
@@ -25,7 +26,7 @@ export default function FormModal({ open, handleClose }:
                             src='https://i.pinimg.com/564x/01/2b/0b/012b0bc2e871fc073c8dbf8008bdf20e.jpg' />
                         <p>Tài phiệt no name</p>
                     </div>
-                    <form className='flex flex-col items-center'>
+                    <form className='flex flex-col items-center p-4'>
                         <div className={classes.textbox}>
                             <div
                                 className={classes.content}
@@ -34,8 +35,14 @@ export default function FormModal({ open, handleClose }:
                                 contentEditable="true"
                                 aria-multiline="true"
                                 aria-labelledby="txtboxMultilineLabel"
-                                aria-required="true"></div>
+                                aria-required="true">
+
+                            </div>
+
                         </div>
+                        <FormModalImagePicker />
+
+
                         <button className={classes.share_button}>Share</button>
                     </form>
 
