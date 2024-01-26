2b5cd9e8f62a5508f23ac305886b98c26aed4422
eung-eung <boyvip5231@gmail.com>
2024-01-27 01:54:35 +0700

    clean import

--- src/app/(user)/explores/@modal/(..)explores/[id]/page.tsx
+++ src/app/(user)/explores/@modal/(..)explores/[id]/page.tsx
@@ -1,13 +1,13 @@
 import Modal from '@/app/components/explorePage/modal'
 
-import photos, { Photo } from '@/app/lib/photos'
+
 
 export default function PhotoModal({
   params: { id }
 }: {
   params: { id: string }
 }) {
-  const photo: Photo = photos.find(p => p.id === id)!
+
 
   return (
     <Modal>
