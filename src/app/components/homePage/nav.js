061f2f9042629d265ed1f250abb34d6cc7204b1a
eung-eung <boyvip5231@gmail.com>
2024-01-27 01:57:20 +0700

    fix link href explore

--- src/app/components/homePage/nav.js
+++ src/app/components/homePage/nav.js
@@ -153,7 +153,7 @@ export default function Nav() {
                           <ChevronRightIcon className="h-6 w-6 text-gray-600 group-hover:text-indigo-600" aria-hidden="true" />
                         </div>
                         <div className="flex-auto">
-                          <Link href="/explore" className="block font-semibold text-gray-900">
+                          <Link href="/explores" className="block font-semibold text-gray-900">
                             Explore
                             <span className="absolute inset-0" />
                           </Link>
@@ -327,7 +327,7 @@ export default function Nav() {
                         </div>
                         <div className="flex-auto">
                           <Link href="/listTutor" className="block font-semibold text-gray-900">
-                          List of Tutors
+                            List of Tutors
                             <span className="absolute inset-0" />
                           </Link>
                         </div>
