a435957c6c15eb0db387b6704e4b1b5a9cd01319
Trang <ngotrang2508.forwork@gmail.com>
2024-01-27 22:17:09 +0700

    link history order

--- src/app/(user)/historyOrder/page.tsx
+++ src/app/(user)/historyOrder/page.tsx
@@ -1,6 +1,9 @@
 import HistoryOrderPage from '@/app/components/historyOrderPage/page'
 import React from 'react'
-
+import { Metadata } from 'next'
+export const metadata: Metadata = {
+    title: 'History Order'
+}
 export default function HistoryOrder() {
   return (
     <div >
