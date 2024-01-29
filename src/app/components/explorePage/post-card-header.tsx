4ca08be45f2ccb209ec4b53bfd5339b1f3aee0c9
eung-eung <boyvip5231@gmail.com>
2024-01-29 09:41:49 +0700

    post card header

---
+++ src/app/components/explorePage/post-card-header.tsx
@@ -0,0 +1,18 @@
+'use client'
+import React from 'react'
+import classes from './post-card-header.module.css'
+import CancelIcon from '@mui/icons-material/Cancel';
+
+import { useRouter } from 'next/navigation'
+export default function PostCardHeader() {
+    const router = useRouter()
+    const handleClose = () => router.back()
+
+
+    return (
+        <div className='relative'>
+            <h2 className={classes.title}>This is 39saku_chan's post</h2>
+            <CancelIcon className={classes.close_post} onClick={handleClose} />
+        </div>
+    )
+}
