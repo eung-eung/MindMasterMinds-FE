050ce0d4f99ec883f8e649ef56c9815203797807
eung-eung <boyvip5231@gmail.com>
2024-01-29 09:41:38 +0700

    post card component

--- src/app/components/explorePage/post-card.tsx
+++ src/app/components/explorePage/post-card.tsx
@@ -1,21 +1,17 @@
 import Image from 'next/image'
-
+import classes from './post-card.module.css'
+import PostCardHeader from './post-card-header'
+import PostCardBody from './post-card-body'
+import PostCardInputComment from './post-card-input-comment'
 
 export default function PostCard() {
   return (
-    <>
-      <Image
-        alt=''
-        src='https://i.pinimg.com/564x/01/2b/0b/012b0bc2e871fc073c8dbf8008bdf20e.jpg'
-        height={600}
-        width={600}
-        className='col-span-1 aspect-square w-full object-cover'
-      />
+    <div>
 
-      <div className=' bg-white p-2 px-4'>
-        <h3 className='font-serif text-xl font-medium'>Test</h3>
-        <p className='text-sm text-gray-500'>Taken by </p>
-      </div>
-    </>
+      <PostCardHeader />
+      <PostCardBody />
+      <PostCardInputComment />
+    </div>
+
   )
 }
