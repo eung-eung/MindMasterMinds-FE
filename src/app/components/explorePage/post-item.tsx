1ccb206a61e37ca1b804beaa46f4a7b91fd601eb
eung-eung <boyvip5231@gmail.com>
2024-01-29 09:41:20 +0700

    post item component

--- src/app/components/explorePage/post-item.tsx
+++ src/app/components/explorePage/post-item.tsx
@@ -27,7 +27,7 @@ export default function PostItem() {
             </div>
             <div className={classes.sub_action}>
                 <div className={classes.subaction_box}>
-                    <ThumbUpIcon color='primary' className='mr-2' />
+                    <img src='https://scontent.fsgn5-5.fna.fbcdn.net/m1/v/t6/An_rhVAVeaDMf_VavgOfjtuROilB_OxYlzJB-YfmtmQICTxUx0-ro45L5ecilP3Pd9nrcwS6n0PT2XMF1-cSsXfViIRIw9BBCH6ZLoEFSr6rhDANB8hnK_ICPGF5JVeIZw.png?ccb=10-5&oh=00_AfDdjZLO35O-u3B_O5kIBH1hP4OdCNsOXaCJD-HF1oobFg&oe=65DD8330&_nc_sid=7da55a' />
                     <span>5000</span>
                 </div>
                 <div className={classes.subaction_box}>
