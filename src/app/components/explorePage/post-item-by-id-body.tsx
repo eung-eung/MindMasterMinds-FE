82dfe1e97d6e266a6d93c1722712a074b9dcd4b4
eung-eung <boyvip5231@gmail.com>
2024-01-29 17:46:28 +0700

    post item by id body component

---
+++ src/app/components/explorePage/post-item-by-id-body.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PostCardBodyAction from './post-card-body-action'
+
+export default function PostItemByIdBody() {
+    return (
+
+        <PostCardBodyAction />
+
+    )
+}
