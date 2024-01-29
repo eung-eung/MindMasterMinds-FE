8c7b57db8cd27f0f1ac7d4eb87bf5658ed57d788
eung-eung <boyvip5231@gmail.com>
2024-01-29 09:42:03 +0700

    post card grid component

---
+++ src/app/components/explorePage/post-card-grid-comments.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PostCardComment from './post-card-comment'
+
+export default function PostCardGridComments() {
+    return (
+        <div className='pl-2 mb-5'>
+            <PostCardComment />
+            <PostCardComment />
+            <PostCardComment />
+            <PostCardComment />
+        </div>
+    )
+}
