014449f5e9b76bea82a1e1b36074dad594c8bf07
Trang <ngotrang2508.forwork@gmail.com>
2024-01-31 22:18:03 +0700

    link list of classes

--- src/app/components/signUpPage/sign-up-form.tsx
+++ src/app/components/signUpPage/sign-up-form.tsx
@@ -16,6 +16,7 @@ export default function SignUpForm() {
     const confirmPassword = React.useRef<HTMLInputElement>(null)
     const otp = React.useRef<HTMLInputElement>(null)
     const regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+console.log( process.env.API_KEY);
 
     const isValidMail = (mail: any) => {
         if (regex.test(mail)) {
@@ -49,6 +50,7 @@ export default function SignUpForm() {
             }
         )
         console.log(response);
+        
     }
 
     const registerAccount = async (
@@ -107,13 +109,13 @@ export default function SignUpForm() {
             });
             return
         }
-        // const account: AccountRegister = {}
-        // account.firstName = firstNameValue
-        // account.lastName = lastNameValue
-        // account.password = passwordValue
-        // account.email = emailValue
-        // account.otpEmailCode = otpValue
-        // registerAccount(account)
+        const account: AccountRegister = {}
+        account.firstName = firstNameValue
+        account.lastName = lastNameValue
+        account.password = passwordValue
+        account.email = emailValue
+        account.otpEmailCode = otpValue
+        registerAccount(account)
     }
 
 
