2ba1ffa46a019887065d6ab35b5840bbacd851b1
eung-eung <boyvip5231@gmail.com>
2024-01-31 20:45:20 +0700

    disable reload when sign in

--- src/app/components/header/headerNoAuth.tsx
+++ src/app/components/header/headerNoAuth.tsx
@@ -3,8 +3,13 @@ import React from 'react'
 import classes from './header.module.css'
 import { signIn } from 'next-auth/react'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 export default function HeaderNoAuth() {
+    const router = useRouter()
+    const handleSignIn = () => {
+        router.push('/signIn')
+    }
     return (
         <nav className="relative mx-auto flex max-w-7xl items-center justify-center p-6 lg:px-8" aria-label="Global">
             <div className="flex items-center">
@@ -14,7 +19,7 @@ export default function HeaderNoAuth() {
             </div>
             <button type='button'
                 className={classes.signin_button + ' absolute '}
-                onClick={() => signIn()}
+                onClick={handleSignIn}
             >
                 Sign In
             </button>
