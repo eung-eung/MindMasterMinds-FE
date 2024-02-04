96216f93e04b0d09dff545d2e0b8335d4a4e09de
eung-eung <boyvip5231@gmail.com>
2024-02-04 16:19:13 +0700

    update reactions and comments count props

--- src/app/components/explorePage/post-item.tsx
+++ src/app/components/explorePage/post-item.tsx
@@ -4,8 +4,8 @@ import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
 import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import Link from 'next/link';
-export default function PostItem({ name, content, imageUrl, avatar }
-    : { name: string, content: string, imageUrl: string, avatar: string }) {
+export default function PostItem({ name, content, imageUrl, avatar, reactionsCount, commentsCount }
+    : { name: string, content: string, imageUrl: string, avatar: string, reactionsCount: number, commentsCount: number }) {
     return (
         <div className={classes.post_item + ' w-full border'}>
             <div className={classes.header}>
@@ -29,10 +29,10 @@ export default function PostItem({ name, content, imageUrl, avatar }
             <div className={classes.sub_action}>
                 <div className={classes.subaction_box}>
                     <img src='https://scontent.fsgn5-5.fna.fbcdn.net/m1/v/t6/An_rhVAVeaDMf_VavgOfjtuROilB_OxYlzJB-YfmtmQICTxUx0-ro45L5ecilP3Pd9nrcwS6n0PT2XMF1-cSsXfViIRIw9BBCH6ZLoEFSr6rhDANB8hnK_ICPGF5JVeIZw.png?ccb=10-5&oh=00_AfDdjZLO35O-u3B_O5kIBH1hP4OdCNsOXaCJD-HF1oobFg&oe=65DD8330&_nc_sid=7da55a' />
-                    <span>5000</span>
+                    <span>{reactionsCount}</span>
                 </div>
                 <div className={classes.subaction_box}>
-                    <span>2500 comments</span>
+                    <span>{commentsCount} comments</span>
                 </div>
             </div>
             <div className={classes.action}>
