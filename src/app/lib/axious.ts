04f0877a4147b48529e388b7ee2cb18d0c15a1f3
eung-eung <boyvip5231@gmail.com>
2024-02-04 16:18:51 +0700

    config axios

--- src/app/lib/axious.ts
+++ src/app/lib/axious.ts
@@ -1,6 +1,12 @@
 import axios from "axios";
+console.log(process.env.API);
 
 export default axios.create({
-    baseURL: process.env.API,
+    baseURL: process.env.API_KEY,
+    headers: { "Content-Type": "application/json" }
+})
+
+export const axiosAuth = axios.create({
+    baseURL: process.env.API_KEY,
     headers: { "Content-Type": "application/json" }
 })
