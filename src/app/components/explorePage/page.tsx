3137e5acb4afac140b60bc1c550dfac5cf100ba3
eung-eung <boyvip5231@gmail.com>
2024-02-07 00:01:07 +0700

    clear unuse lines

--- src/app/components/explorePage/page.tsx
+++ src/app/components/explorePage/page.tsx
@@ -1,12 +1,7 @@
 'use client'
 import React from 'react'
-import Header from '@/app/components/homePage/header'
-import LoadingTheme from '@/app/components/loadingTheme/loadingTheme'
-import { useSession } from 'next-auth/react'
 import FormPostBlog from './form-post-blog'
-import PostItem from './post-item'
 import PostGrid from './post-grid'
-import { redirect } from 'next/navigation'
 import useAxiosAuth from '@/app/lib/hooks/useAxiosAuth'
 import { PostExplore } from '@/app/types/Post-Explore'
 
@@ -15,8 +10,7 @@ export default function ExplorePage() {
     const [listPost, setListPost] = React.useState<PostExplore[]>([])
     const [callGetPost, setCallGetPost] = React.useState<Boolean>(true)
     const getPosts = async () => {
-        const response = await axiosAuth.get('/Post?pageNumber=0&pageSize=5')
-
+        const response = await axiosAuth.get('/Post?pageNumber=0&pageSize=100')
         setListPost(response.data.data)
 
     }
