de0b06ca07ce17f7f8373c90cde756a0299a4221
Trang <ngotrang2508.forwork@gmail.com>
2024-02-07 20:02:11 +0700

    new

--- src/app/(user)/tutors/page.tsx
+++ src/app/(user)/tutors/page.tsx
@@ -2,7 +2,6 @@ import BecomeTutorPage from '@/app/components/becomeTutorPage/page'
 import React from 'react'
 
 import { Metadata } from 'next'
-import TutorDashboard from '@/app/components/tutorDashboardPage/page'
 export const metadata: Metadata = {
     title: 'Become The Tutor'
 }
@@ -11,7 +10,6 @@ export default function BecomeTutor() {
   return (
     <div>
         <BecomeTutorPage />
-        <TutorDashboard/>
     </div>
   )
 }
