6b74e8342f175d9115fef90510919938eac137a6
eung-eung <boyvip5231@gmail.com>
2024-02-09 01:38:54 +0700

    intergrate api for post a comment

--- src/app/components/explorePage/post-card-input-comment.tsx
+++ src/app/components/explorePage/post-card-input-comment.tsx
@@ -4,13 +4,16 @@ import classes from './post-card-input-comment.module.css'
 import SendIcon from '@mui/icons-material/Send';
 import { useSession } from 'next-auth/react';
 import Image from 'next/image';
-export default function PostCardInputComment() {
+import { Bounce, ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import useAxiosAuth from '@/app/lib/hooks/useAxiosAuth';
+export default function PostCardInputComment({ postId, setIsRefresh }: { postId: any, setIsRefresh: any }) {
     const inputField = React.useRef<HTMLDivElement>(null)
+    const [isLoading, setIsLoading] = React.useState(false)
     const { data: session } = useSession()
     const [comment, setComment] = React.useState(String)
-    const handleComment = () => {
-        console.log(inputField.current?.innerHTML);
-    }
+    const axiosAuth = useAxiosAuth()
+
     const handleChange = () => {
         if (inputField && inputField.current?.innerHTML) {
             setComment(inputField.current?.innerHTML)
@@ -19,11 +22,61 @@ export default function PostCardInputComment() {
         }
     }
 
+    const handlePostComment = async () => {
 
+        console.log(inputField.current?.innerText.trim().length);
+        if (inputField.current && inputField.current.innerText.trim().length <= 0) {
+            toast.error('Do not have content', {
+                position: "top-center",
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+                transition: Bounce,
+            });
+            return
+        } else {
+            setIsLoading(true)
+            const response = await axiosAuth.post('/PostComment', {
+                postId: postId,
+                comment: inputField.current?.innerHTML
+            })
+
+            toast.success('Posted sucessfully', {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+                transition: Bounce,
+            });
+            setIsRefresh((prev: any) => !prev)
+            setIsLoading(false)
+        }
+
+    }
     return (
         <>
+            <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+                transition={Bounce}
+            />
             <div className={classes.box}>
-                <div>
+                <div className={classes.box_1}>
                     <Image
                         alt={session?.user.userViewLogin.firstName + '' + session?.user.userViewLogin.lastName}
                         className={classes.avatar}
@@ -44,8 +97,13 @@ export default function PostCardInputComment() {
                     aria-required="true">
 
                 </div>
-                <SendIcon onClick={handleComment}
-                    color={comment ? 'primary' : 'disabled'} />
+                {isLoading ?
+                    <span className={classes.loader}></span>
+                    :
+                    <SendIcon onClick={handlePostComment}
+                        color={comment ? 'primary' : 'disabled'} />
+                }
+
 
             </div >
         </>
