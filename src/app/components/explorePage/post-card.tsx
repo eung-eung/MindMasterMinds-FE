8652fc48f820d063a6fa9235d5a9d3bd104550de
eung-eung <boyvip5231@gmail.com>
2024-02-08 23:19:39 +0700

    Integrate API  to get a post

--- src/app/components/explorePage/post-card.tsx
+++ src/app/components/explorePage/post-card.tsx
@@ -3,13 +3,16 @@ import classes from './post-card.module.css'
 import PostCardHeader from './post-card-header'
 import PostCardBody from './post-card-body'
 import PostCardInputComment from './post-card-input-comment'
+import { PostExplore } from '@/app/types/Post-Explore'
 
-export default function PostCard() {
+export default function PostCard({ post }: { post: PostExplore }) {
+  console.log('post: ', post.user);
+
   return (
     <div>
 
-      <PostCardHeader />
-      <PostCardBody />
+      <PostCardHeader post={post} />
+      <PostCardBody post={post} />
       <PostCardInputComment />
     </div>
 
