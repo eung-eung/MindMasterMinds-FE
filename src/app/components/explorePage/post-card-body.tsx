ff9b7b32ea3595d99354c347542ecbbbc7250ac0
eung-eung <boyvip5231@gmail.com>
2024-02-08 23:13:11 +0700

    pass props to render in card body

--- src/app/components/explorePage/post-card-body.tsx
+++ src/app/components/explorePage/post-card-body.tsx
@@ -1,38 +1,64 @@
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import classes from './post-card-body.module.css'
 import PostCardBodyAction from './post-card-body-action'
 import PostCardGridComments from './post-card-grid-comments'
-export default function PostCardBody() {
+import { PostExplore } from '@/app/types/Post-Explore'
+import moment from 'moment-timezone'
+import { Skeleton } from '@mui/material'
+export default function PostCardBody({ post }: { post: PostExplore }) {
+    const [isLoading, setIsLoading] = useState(true)
+
     return (
         <div className={classes.body}>
             <div className={classes.header}>
                 <div className={classes.post_user}>
-                    <img src='/images/linda.jpg' className='object-cover' />
+                    <Image src={post.user.avatar || '/images/noAvatar.jpg'} className='object-cover'
+                        width={50} height={50}
+                        alt={post.user.firstName + ' ' + post.user.lastName}
+                    />
                     <div className={classes.right_post_user}>
-                        <p className={classes.right_post_username}>Linda</p>
-                        <p className={classes.right_post_user_create}>17/1/2024</p>
+                        <p className={classes.right_post_username}>{post.user.firstName + ' ' + post.user.lastName}</p>
+                        <p className={classes.right_post_user_create}>
+                            {
+                                moment.utc(post.creationDate).tz('Asia/Ho_Chi_Minh').format('DD-MM-YYYY HH:mm:ss')
+
+                            }
+                        </p>
                     </div>
                 </div>
 
                 <div className={classes.post_content}>
-                    Pachapupa, woa, Kim chi đỏ auuuu, thơm phức í haaaaaa
+                    {post.content}
                 </div>
-                <Image
-                    alt=''
-                    src='https://i.pinimg.com/564x/01/2b/0b/012b0bc2e871fc073c8dbf8008bdf20e.jpg'
-                    height={600}
-                    width={600}
-                    style={{ background: 'antiquewhite' }}
-                    className='col-span-1 aspect-square w-full'
-                />
+                {
+                    post.image &&
+                    <Image
+                        alt=''
+                        src={post.image}
+                        height={600}
+                        width={600}
+                        style={{ background: 'antiquewhite' }}
+                        loading="lazy"
+                        onLoadingComplete={() => setIsLoading(false)}
+                        className={
+                            isLoading
+                                ? 'opacity-0' : 'opacity-100' + ' col-span-1 aspect-square w-full'}
+                    />
+                }
+                {
+                    isLoading && (
+                        <Skeleton
+                            sx={{ bgcolor: 'grey.100' }}
+                            variant="rectangular"
+                            width="100%"
+                            height="100%"
+                        />
+                    )
+                }
             </div>
             <PostCardBodyAction />
             <PostCardGridComments />
-            {/* <div className=' bg-white p-2 px-4'>
-                <h3 className='font-serif text-xl font-medium'>Test</h3>
-                <p className='text-sm text-gray-500'>Taken by </p>
-            </div> */}
         </div>
     )
 }
