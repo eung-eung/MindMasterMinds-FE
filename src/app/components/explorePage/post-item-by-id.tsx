0f60b19123420f6df615c0e2c196e18e182005b1
eung-eung <boyvip5231@gmail.com>
2024-02-08 23:22:06 +0700

    Integrate API to show 1 post by id

--- src/app/components/explorePage/post-item-by-id.tsx
+++ src/app/components/explorePage/post-item-by-id.tsx
@@ -1,13 +1,50 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect } from 'react'
 import PostItemByIdRight from './post-item-by-id-right'
 import classes from './post-item-by-id.module.css'
-export default function PostItemById() {
+import { PostExplore } from '@/app/types/Post-Explore'
+import useAxiosAuth from '@/app/lib/hooks/useAxiosAuth'
+import Image from 'next/image'
+export default function PostItemById({ id }: { id: any }) {
+    const [post, setPost] = React.useState<PostExplore | any>()
+    const [isLoading, setIsLoading] = React.useState(true)
+    const axiosAuth = useAxiosAuth()
+    const getPost = async () => {
+        setIsLoading(isLoading)
+        const response = await axiosAuth.get(`/Post/${id}`)
+        setPost(response.data)
+        setIsLoading(false)
+    }
+    useEffect(() => {
+        const controller = new AbortController()
+        getPost()
+        return () => controller.abort()
+    }, [])
+
     return (
-        <div className={classes.box}>
-            <div className={classes.post_image}>
-                <img src='https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?cs=srgb&dl=pexels-james-wheeler-414612.jpg&fm=jpg' />
-            </div>
-            <PostItemByIdRight />
-        </div>
+        <>
+
+            {
+                isLoading ?
+                    <p>Loading</p>
+                    :
+                    <div className={classes.box}>
+                        {
+                            post.image &&
+                            <div className={classes.post_image}>
+                                <img
+                                    src={post.image}
+
+                                />
+                            </div>
+                        }
+
+                        <PostItemByIdRight post={post} />
+                    </div>
+            }
+        </>
+
+
     )
 }
