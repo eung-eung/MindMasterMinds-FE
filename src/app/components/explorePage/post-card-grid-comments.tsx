750e161eea505887d5b86e9a06e7e72d3db8149b
eung-eung <boyvip5231@gmail.com>
2024-02-08 23:13:37 +0700

    add more fixed comments

--- src/app/components/explorePage/post-card-grid-comments.tsx
+++ src/app/components/explorePage/post-card-grid-comments.tsx
@@ -8,6 +8,8 @@ export default function PostCardGridComments() {
             <PostCardComment />
             <PostCardComment />
             <PostCardComment />
+            <PostCardComment />
+            <PostCardComment />
         </div>
     )
 }
