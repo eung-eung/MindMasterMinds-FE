244734ae5ff07426735eee42231a60507f64953c
eung-eung <boyvip5231@gmail.com>
2024-02-09 01:40:00 +0700

    show data after call api

--- src/app/components/explorePage/post-card-comment.tsx
+++ src/app/components/explorePage/post-card-comment.tsx
@@ -1,17 +1,30 @@
 import React from 'react'
 import classes from './post-card-comment.module.css'
-export default function PostCardComment() {
+import { CommentPostExplore } from '@/app/types/CommentPostExplore'
+import moment from 'moment-timezone'
+export default function PostCardComment({ comment }: { comment: CommentPostExplore }) {
     return (
         <div className={classes.post_comment}>
-            <img className={classes.guest_avatar} src='/images/guest1.png' />
+            <img className={classes.guest_avatar} src={comment.user.avatar || '/images/noAvatar.jpg'} />
             <div className={classes.box}>
                 <div className={classes.guest_name}>
-                    <span>Yunjin</span>
+                    <span>{comment.user.firstName + ' ' + comment.user.lastName}</span>
+                    <span className={classes.creationDate}>
+                        {
+                            moment.utc(comment.creationDate).tz('Asia/Ho_Chi_Minh').format('DD-MM-YYYY HH:mm:ss')
+                        }
+                    </span>
+
                 </div>
                 <div className={classes.guest_comment}>
-                    Mị chọt xố? JinJa
+                    <div className={classes.body_content}
+                        dangerouslySetInnerHTML={{ __html: comment.comment }}>
+
+                    </div>
+
                 </div>
             </div>
+
         </div>
     )
 }
