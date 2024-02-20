038e565d1776bf0e39b77ab8f0d728460b6b65f6
eung-eung <boyvip5231@gmail.com>
2024-02-20 15:03:25 +0700

    interface order

---
+++ src/app/types/Order.d.ts
@@ -0,0 +1,10 @@
+export interface Order {
+    order: any;
+    summary: string,
+    courseSubjectId: string,
+    stateInfo: boolean,
+    phone: string,
+    description: string,
+    quantity: number,
+    study: Date
+}
