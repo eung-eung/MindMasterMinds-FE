42c16f726c4e7e2b744e6e9dcc4fe8c81e0dd484
eung-eung <boyvip5231@gmail.com>
2024-02-21 05:03:58 +0700

    add layout for class

---
+++ src/app/(user)/listClasses/[classId]/layout.tsx
@@ -0,0 +1,36 @@
+'use client'
+import Footer from '@/app/components/footer/footer'
+import Header from '@/app/components/homePage/header'
+import LoadingTheme from '@/app/components/loadingTheme/loadingTheme'
+import { useSession } from 'next-auth/react'
+import { redirect } from 'next/navigation'
+import React from 'react'
+export default function AClassLayout({
+  children,
+  modal
+}: {
+  children: React.ReactNode
+  modal: React.ReactNode
+}) {
+  const { data: session, status } = useSession()
+  const [isLoading, setLoading] = React.useState<boolean>(true)
+  console.log(session);
+
+
+  React.useEffect(() => {
+    if (status !== 'loading') setLoading(false)
+    if (!session && status === 'unauthenticated') {
+      redirect('/signIn')
+    }
+  }, [status])
+  return (
+    <>
+      {
+        isLoading ? <LoadingTheme /> : <>
+          <Header title='Class' isHome={false} />
+          {children}
+          <Footer />
+        </>}
+    </>
+  )
+}
