8aaa6f47e69bb71f60455f296d67986786fc6b13
eung-eung <boyvip5231@gmail.com>
2024-02-21 16:54:48 +0700

    tutor item component

--- src/app/components/listClassesPage/tutorItem.tsx
+++ src/app/components/listClassesPage/tutorItem.tsx
@@ -21,6 +21,66 @@ export default function TutorItem({ tutor }: { tutor: Tutor }) {
                 </div>
                 <button className={classes.approve_btn} type='button'>Approve</button>
             </div>
+            <div className={classes.tutor_card}>
+                <img className={classes.left_content} src={tutor.avatar} />
+                <div className={classes.right_content}>
+                    <div className={classes.card_item + ' ' + classes.tutor_name}>
+                        {tutor.firstName + ' ' + tutor.lastName}
+                    </div>
+                    <div className={classes.card_item + ' ' + classes.tutor_mail}>
+                        <EmailOutlinedIcon />     {tutor.email}
+                    </div>
+                    <div className={classes.card_item + ' ' + classes.tutor_role}>
+                        {tutor.userRole.roleName}
+                    </div>
+                </div>
+                <button className={classes.approve_btn} type='button'>Approve</button>
+            </div>
+            <div className={classes.tutor_card}>
+                <img className={classes.left_content} src={tutor.avatar} />
+                <div className={classes.right_content}>
+                    <div className={classes.card_item + ' ' + classes.tutor_name}>
+                        {tutor.firstName + ' ' + tutor.lastName}
+                    </div>
+                    <div className={classes.card_item + ' ' + classes.tutor_mail}>
+                        <EmailOutlinedIcon />     {tutor.email}
+                    </div>
+                    <div className={classes.card_item + ' ' + classes.tutor_role}>
+                        {tutor.userRole.roleName}
+                    </div>
+                </div>
+                <button className={classes.approve_btn} type='button'>Approve</button>
+            </div>
+            <div className={classes.tutor_card}>
+                <img className={classes.left_content} src={tutor.avatar} />
+                <div className={classes.right_content}>
+                    <div className={classes.card_item + ' ' + classes.tutor_name}>
+                        {tutor.firstName + ' ' + tutor.lastName}
+                    </div>
+                    <div className={classes.card_item + ' ' + classes.tutor_mail}>
+                        <EmailOutlinedIcon />     {tutor.email}
+                    </div>
+                    <div className={classes.card_item + ' ' + classes.tutor_role}>
+                        {tutor.userRole.roleName}
+                    </div>
+                </div>
+                <button className={classes.approve_btn} type='button'>Approve</button>
+            </div>
+            <div className={classes.tutor_card}>
+                <img className={classes.left_content} src={tutor.avatar} />
+                <div className={classes.right_content}>
+                    <div className={classes.card_item + ' ' + classes.tutor_name}>
+                        {tutor.firstName + ' ' + tutor.lastName}
+                    </div>
+                    <div className={classes.card_item + ' ' + classes.tutor_mail}>
+                        <EmailOutlinedIcon />     {tutor.email}
+                    </div>
+                    <div className={classes.card_item + ' ' + classes.tutor_role}>
+                        {tutor.userRole.roleName}
+                    </div>
+                </div>
+                <button className={classes.approve_btn} type='button'>Approve</button>
+            </div>
 
 
 
