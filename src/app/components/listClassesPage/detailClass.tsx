0e4658eaf3de37e29357388634d60cc6875cad18
eung-eung <boyvip5231@gmail.com>
2024-02-21 16:54:31 +0700

    detial class component

--- src/app/components/listClassesPage/detailClass.tsx
+++ src/app/components/listClassesPage/detailClass.tsx
@@ -1,16 +1,18 @@
 'use client'
-import useAxiosAuth from '@/app/lib/hooks/useAxiosAuth'
+
 import { Tutor } from '@/app/types/Tutor'
-import React from 'react'
+import { useEffect, useState } from 'react'
 import GridListTutors from './gridListTutors'
 import classes from './detailClass.module.css'
+import { useSession } from 'next-auth/react'
 export default function DetailClass({ id }: { id: any }) {
+    const { data: session } = useSession()
 
 
 
     return (
         <div>
-            <GridListTutors id={id} />
+            <GridListTutors role={session?.user.userViewLogin.userRole.roleName} id={id} />
         </div>
     )
 }
